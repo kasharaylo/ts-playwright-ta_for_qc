8f9597e4c93fb242c2b97eadbd0e0e8584cc8abf
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-06-16 16:15:16 +0300

    api registration

---
+++ tests/apiRegistration.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test'
+import { userData } from '../testData/users.data'
+import * as fs from 'fs/promises'
+import * as path from 'path'
+
+const userFilePath = path.resolve(__dirname, '../testData/registeredUser.json')
+const tokenFilePath = path.resolve(__dirname, '../testData/tokenUser.json')
+
+test('API Sign Up', async ({ request }) => {
+const response = await request.post('https://gitlab.testautomate.me/api/v4/users', {
+  headers: {
+    Authorization: 'Bearer FKzy_BpV5wAybKf7Z9JX',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  form: {
+    name: userData.firstName,
+    username: userData.userName,
+    email: userData.email,
+    password: userData.password,
+    skip_confirmation: true,
+  }
+});
+    expect(response.status()).toBe(201);
+
+    await fs.writeFile(userFilePath, JSON.stringify({
+        email: userData.email,
+        password: userData.password
+    }, null, 2))
+});
+
+test('API Sign In', async ({ request }) => {
+    const userData = await fs.readFile(userFilePath, 'utf-8')
+    const registeredUser = JSON.parse(userData)
+    
+    const response = await request.post('https://gitlab.testautomate.me/oauth/token', {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            grant_type: 'password',
+            username: registeredUser.email,
+            password: registeredUser.password
+        }
+    });
+
+    expect(response.status()).toBe(200);
+
+    const responseBody = await response.json();
+    expect(responseBody).toHaveProperty('access_token');
+
+    // Save registered user info to file
+    await fs.writeFile(tokenFilePath, JSON.stringify({
+        access_token: responseBody.access_token
+    }, null, 2))
+});
